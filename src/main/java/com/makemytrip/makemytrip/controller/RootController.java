17e35f83cb565bf07ba4a84aaed0e5b0e0ba7566
Raj0825 <shahraj0805@gmail.com>
2026-06-26 21:59:14 +0530

    added cross origins

--- src/main/java/com/makemytrip/makemytrip/controller/RootController.java
+++ src/main/java/com/makemytrip/makemytrip/controller/RootController.java
@@ -1,9 +1,11 @@
 package com.makemytrip.makemytrip.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class RootController {
     @GetMapping("/")
             public String home() {
