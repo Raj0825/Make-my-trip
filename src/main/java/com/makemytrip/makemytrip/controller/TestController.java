a46b8b3532359cd6ebcb532457de3243c0b21290
Raj0825 <shahraj0805@gmail.com>
2026-07-02 22:07:40 +0530

    new values

--- src/main/java/com/makemytrip/makemytrip/controller/TestController.java
+++ src/main/java/com/makemytrip/makemytrip/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.makemytrip.makemytrip.controller;
 
-import com.makemytrip.makemytrip.repository.UserRepository;
+import com.makemytrip.makemytrip.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
