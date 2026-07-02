31ac95c9d9d2ee53350e3689bc0830c8edfafaa3
Raj0825 <shahraj0805@gmail.com>
2026-07-02 21:25:24 +0530

    minor updates

--- makemytour/next.config.ts
+++ makemytour/next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/Make-my-trip",
+  assetPrefix: "/Make-my-trip/",
 };
 
 export default nextConfig;
