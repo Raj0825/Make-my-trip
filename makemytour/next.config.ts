fcc6855e1418f9eb982e21c7da85dff623633c4b
Raj0825 <shahraj0805@gmail.com>
2026-07-03 14:51:45 +0530

    Configure Next.js for Vercel

--- makemytour/next.config.ts
+++ makemytour/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/Make-my-trip",
-  assetPrefix: "/Make-my-trip/",
 };
 
 export default nextConfig;
